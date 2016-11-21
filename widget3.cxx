d07e9b90bc8c21a676fca7ed34908027ef55297c
Anusha P S <anusha@Anushas-MacBook-Pro.local>
2016-11-22 00:36:43 +0530

    New widget

--- widget3.cxx
+++ widget3.cxx
@@ -1,25 +1,3 @@
-/*
-#include "widget3.h"
-#include "ui_widget3.h"
-#include "controller.h"
-
-Widget3::Widget3(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Widget3)
-{
-    ui->setupUi(this);
-}
-
-Widget3::~Widget3()
-{
-    delete ui;
-}
-
-void Widget3::setController(Controller* c) {
-    controller = c;
-}
-*/
-
 #include "widget3.h"
 #include "ui_widget3.h"
 #include "controller.h"
